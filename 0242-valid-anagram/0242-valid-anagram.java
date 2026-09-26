95256c44f645cd339646a936e78092c9bf1c9c77
abhishek15-abhi <alabhishek15@gmail.com>
2026-09-26 15:03:19 +0530

    Time: 4 ms (83.7%), Space: 46.7 MB (8.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        s.toLowerCase();
-        t.toUpperCase();
-        char[] string1=s.toCharArray();
-        char[] string2=t.toCharArray();
-        Arrays.sort(string1);
-        Arrays.sort(string2);
-        if(s.length()==t.length())
-        {
-            if(Arrays.equals(string2,string1))
-                return true;
-        }
-        return false;
+        char[] a =s.toCharArray();
+        char[] b=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
     }
 }
